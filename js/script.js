a3703065afc8a9bb2a6269f244e1fa34c7efcb90
sylvester omondi <sylvesteromondi49@gmail.com>
2018-09-18 16:33:08 +0300

    add botstrap css index.html file and js script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+});
